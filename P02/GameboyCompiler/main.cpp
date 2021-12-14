960286e06917f7b4e2267976588b00a69f5156c4
Jonas Otto <jottosmail@gmail.com>
2021-12-14 15:01:22 +0100

    parser

--- P02/GameboyCompiler/main.cpp
+++ P02/GameboyCompiler/main.cpp
@@ -1,15 +1,14 @@
 #include <iostream>
 #include <antlr4-runtime.h>
 #include "GameboyLanguageLexer.h"
+#include "GameboyLanguageParser.h"
 
 int main() {
-    antlr4::ANTLRInputStream input(std::string("y = add (x,3)"));
-    gbparser::GameboyLanguageLexer l(&input);
-    for (auto token = l.nextToken();
-         token != nullptr and token->getType() != gbparser::GameboyLanguageLexer::EOF;
-         token = l.nextToken()) {
-        std::cout << token->getText();
-    }
-    std::cout << std::endl;
+    antlr4::ANTLRInputStream input(std::string("y = add (x,3);"));
+    gbparser::GameboyLanguageLexer lexer(&input);
+    antlr4::CommonTokenStream tokenStream(&lexer);
+    gbparser::GameboyLanguageParser parser(&tokenStream);
+    antlr4::tree::ParseTree *parseTree = parser.program();
+    std::cout << parseTree->toStringTree(&parser, true) << std::endl;
     return 0;
 }
